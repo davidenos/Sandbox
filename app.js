c703f57e8c813d0c4efdc75385fb7129affd06ae
YesImAaron <acochran89@gmail.com>
2014-06-23 00:38:40 -0500

    Create app.js
    javascript for the submit for alert

---
+++ app.js
@@ -0,0 +1,7 @@
+$("#fbLogin").click(function(){
+    alert("this would take you to the facebook Oauth dialog, which requires an app id")
+});
+
+$("#messageForm").submit(function(){
+    alert("Your Message has been sent!");
+})
